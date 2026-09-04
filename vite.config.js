80e4085ad35eb3dd824fa092bc4dbc9af5db3d99
Luca Kessing <lucakessing@gmail.com>
2026-09-04 14:02:50 +0200

    push

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'node:path';
 
 export default defineConfig({
+  base: '/routeplanner/',
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
